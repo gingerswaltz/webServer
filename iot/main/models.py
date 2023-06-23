7c72e025d5473f23486fd122feda6ac973e8e33d
gingerswaltz <goshan712@mail.ru>
2023-06-23 21:22:50 +0900

    a lot of (descr)
    Поменяна бд:
    Теперь pk - installation number. id убран
    Сделана таблица ip, связь с Reading через FK
    Добавлено поле в таблицу ip, сделано условное обозначение "Обновить" или "Создать запись"...

--- iot/main/models.py
+++ iot/main/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 class Reading(models.Model):
-    id = models.AutoField(primary_key=True)
-    installation_number = models.IntegerField()
+    installation_number = models.IntegerField(primary_key=True)
     date = models.DateField()
     time = models.TimeField()
     generated_power = models.FloatField()
     consumed_power = models.FloatField()
     vertical_position = models.IntegerField()
     horizontal_position = models.IntegerField()
+    ip = models.ForeignKey('Ip', on_delete=models.CASCADE, null=True, blank=True)
+
+class Ip(models.Model):
+    ip_address = models.CharField(max_length=15, default='')
+    port = models.CharField(max_length=5, default='')
