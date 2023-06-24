bd146352eff7ff2c4d205703bb24059d10470bc3
gingerswaltz <goshan712@mail.ru>
2023-06-24 13:12:00 +0900

    bugfix for client
    added int cast for port

--- tcp/client.py
+++ tcp/client.py
@@ -1,12 +1,16 @@
 import socket
 import json
 import argparse
+print("Run with --tcp-host ip  or  --tcp-port port, where ip  and port is that is u need. default is localhost:1024")
 
 # Настройки TCP-сервера
 parser = argparse.ArgumentParser(description='Server Configuration')
-parser.add_argument('--tcp-host', type=str, default='localhost', help='Tcp host')
+parser.add_argument('--tcp-host', type=str, default='127.0.0.1', help='Tcp host')
 parser.add_argument('--tcp-port', type=str, default='1024', help='Tcp port')
 args = parser.parse_args()
+
+
+
 
 tcp_host = args.tcp_host
 tcp_port = args.tcp_port
@@ -14,7 +18,7 @@ tcp_port = args.tcp_port
 # Данные для отправки
 data = {}
 
-data['installation_number']=input('Номер установки: ') or '2'
+data['installation_number']=input('Номер установки: ')
 data['date']=input('Дата (дд.мм.гг): ') or '20.03.2020'
 data['time']=input('Время (чч:мм): ') or '15:30'
 data['generated_power']=input('Сгенерированная мощ-ть: ') or '300'
@@ -30,7 +34,8 @@ tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     # Подключение к серверу
-    tcp_socket.connect((tcp_host, tcp_port))
+
+    tcp_socket.connect((tcp_host, int(tcp_port)))
     socket_address = tcp_socket.getsockname()
     address = socket_address[0]
     port = socket_address[1]
