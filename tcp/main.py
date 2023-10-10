b2698a9eb7f75a7ca9baaa98fc94bf62069d1ef6
gingerswaltz <goshan712@mail.ru>
2023-10-10 17:05:25 +0900

    Update main.py

--- tcp/main.py
+++ tcp/main.py
@@ -3,29 +3,26 @@ import socket
 # Создание соксового сервера
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('85.193.80.133', 1024))  # Пример адреса и порта
-server_socket.listen(5)
+server_socket.listen(1)  # Ожидание одного клиента
 
-while True:
-    print("Ожидание подключения...")
-    client_socket, client_address = server_socket.accept()
-    print(f"Подключено к {client_address}")
+print("Ожидание подключения...")
+client_socket, client_address = server_socket.accept()
+print(f"Подключено к {client_address}")
 
-    # Получение локального IP-адреса и порта соксового соединения
-    local_ip = client_socket.getsockname()[0]
-    local_port = client_socket.getsockname()[1]
-    
-    print(f"Локальный IP-адрес: {local_ip}")
-    print(f"Локальный порт: {local_port}")
-
-    # Обработка данных с клиента и отправка ввода с клавиатуры клиенту
+try:
     while True:
-        data = client_socket.recv(1024)
-        if not data:
+        # Ввод с клавиатуры
+        message = input("Введите сообщение для отправки клиенту (для завершения введите 'exit'): ")
+        if message.lower() == 'exit':
             break
-        print(f"Получено от клиента: {data.decode()}")
-        
-        # Ввод с клавиатуры и отправка клиенту
-        message = input("Введите сообщение для отправки клиенту: ")
+
+        # Отправка данных клиенту
         client_socket.sendall(message.encode())
-    
+
+except KeyboardInterrupt:
+    print("Программа завершена пользователем.")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
+finally:
     client_socket.close()
+    server_socket.close()
