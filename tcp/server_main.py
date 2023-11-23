9cfdccf1e7f329d3f6de002bb38bbfc96fe37b54
gingerswaltz <goshan712@mail.ru>
2023-11-23 16:52:31 +0900

    Отмена ошибочных коммитов

--- tcp/server_main.py
+++ tcp/server_main.py
@@ -1,11 +1,12 @@
 import asyncio
+import logging
 import threading
 from TCPServer import TCPServer
-<<<<<<< HEAD
-import sys
-=======
 from aiohttp import web
 import json
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Функция для запуска HTTP-сервера
 async def start_http_server(server):
@@ -14,7 +15,6 @@ async def start_http_server(server):
         web.get('/clients', lambda request: handle_clients(request, server)),
         web.post('/set_active_client', lambda request: handle_set_active_client(request, server)),
         web.post('/send_message', lambda request: handle_send_message(request, server)),
-        web.post('/stop_server', lambda request: handle_stop_server(request, server))
     ])
     runner = web.AppRunner(app)
     await runner.setup()
@@ -23,69 +23,38 @@ async def start_http_server(server):
 
 # Обработчики запросов
 async def handle_clients(request, server):
-    client_list = {client_id: server.connections_mapping[client_id].writer.get_extra_info('peername') for client_id in server.connections_mapping}
-    return web.Response(text=json.dumps(client_list))
+    try:
+        client_list = {client_id: server.connections_mapping[client_id].writer.get_extra_info('peername') for client_id in server.connections_mapping}
+        return web.Response(text=json.dumps(client_list))
+    except Exception as e:
+        logging.error(f"Error handling clients request: {e}")
+        return web.Response(status=500)
 
 async def handle_set_active_client(request, server):
-    data = await request.json()
-    client_id = data.get('client_id')
-    server.set_active_connection(client_id)
-    return web.Response(text=f"Активный клиент установлен: {client_id}")
+    try:
+        data = await request.json()
+        client_id = data.get('client_id')
+        server.set_active_connection(int(client_id))
+        return web.Response(text=f"Активный клиент установлен: {client_id}")
+    except Exception as e:
+        logging.error(f"Error setting active client: {e}")
+        return web.Response(status=500)
 
 async def handle_send_message(request, server):
-    data = await request.json()
-    message = data.get('message')
-    await server.send_message(message)
-    return web.Response(text="Сообщение отправлено")
+    try:
+        data = await request.json()
+        message = data.get('message')
+        await server.send_message(message)
+        return web.Response(text="Сообщение отправлено")
+    except Exception as e:
+        logging.error(f"Error sending message: {e}")
+        return web.Response(status=500)
 
-async def handle_stop_server(request, server):
-    await server.stop_server()
-    return web.Response(text="Сервер остановлен")
 
->>>>>>> parent of cd0828a (HTTP интеграция произведена успешно.)
 
 def start_server_loop(loop, server):
     asyncio.set_event_loop(loop)
     loop.run_until_complete(server.start_server())
-
-def user_interface(server, loop):
-    while True:
-        try:
-            print("\nДоступные команды:")
-            print("1: Показать список подключенных клиентов")
-            print("2: Выбрать клиента для отправки сообщения")
-            print("3: Отправить сообщение выбранному клиенту")
-            print("4: Остановить сервер и выйти")
-            choice = input("Введите команду: ")
-
-            if choice == '1':
-                for client_id in server.connections_mapping:
-                    address = server.connections_mapping[client_id].writer.get_extra_info('peername')
-                    print(f"ID: {client_id}, Address: {address}")
-
-
-            elif choice == '2':
-                for client_id in server.connections_mapping:
-                    address = server.connections_mapping[client_id].writer.get_extra_info('peername')
-                    print(f"ID: {client_id}, Address: {address}")
-                client_id = input("Введите ID клиента: ")
-                try:
-                    server.set_active_connection(int(client_id))
-                except ValueError:
-                    print("Ошибка: Введите корректный числовой ID клиента.")
-
-            elif choice == '3':
-                message = input("Введите сообщение: ")
-                asyncio.run_coroutine_threadsafe(server.send_message(message), loop)
-
-            elif choice == '4':
-                asyncio.run_coroutine_threadsafe(server.stop_server(), loop).result()
-                break
-
-        except Exception as e:
-            print(f"Произошла ошибка: {e}")
-
-    sys.exit(0)
 
 if __name__ == "__main__":
     database_config = {
@@ -98,10 +67,9 @@ if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     server_thread = threading.Thread(target=start_server_loop, args=(loop, server))
     server_thread.start()
-<<<<<<< HEAD
 
-    user_interface(server, loop)
-=======
     # Запуск HTTP сервера в том же цикле событий
     asyncio.run_coroutine_threadsafe(start_http_server(server), loop)
->>>>>>> parent of cd0828a (HTTP интеграция произведена успешно.)
+
+    # Дождаться завершения работы сервера
+    server_thread.join()
