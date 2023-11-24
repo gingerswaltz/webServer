e422e8b62a4726693c7aa47d285275bff762839f
DieSunn <107167630+DieSunn@users.noreply.github.com>
2023-11-25 02:37:51 +0900

    Добавлена тёмная тема
    Есть небольшие баги с расположением тоггла

--- iot/main/static/js/main.js
+++ iot/main/static/js/main.js
@@ -1,7 +1,13 @@
 const body = document.querySelector("body"),
       sidebar = body.querySelector(".sidebar"),
       main_content = body.querySelector(".main-content"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text"),
       toggle = body.querySelector(".toggle");
+
+      modeSwitch.addEventListener("click", () => {
+        body.classList.toggle("dark");
+      });
 
       toggle.addEventListener("click", () => {
         sidebar.classList.toggle("close");
